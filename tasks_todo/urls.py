527955e8af088600fa04b18f9c3b28836348bc6e
KaranSingh07 <karansinghsolanki7410@gmail.com>
2020-07-29 16:23:15 +0530

    Added links to pages using django's methods

--- tasks_todo/urls.py
+++ tasks_todo/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 # It will be used in <a> element to link pages, used to uniquely
-app_name = "tasks_todo"
 # identify the urls of a particular app
+app_name = "tasks_todo"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("add", views.add, name="add")
