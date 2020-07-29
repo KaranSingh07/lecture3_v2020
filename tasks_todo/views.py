d48c1b2209f4d5f00904b22fdfa7a316661468b6
KaranSingh07 <karansinghsolanki7410@gmail.com>
2020-07-29 15:30:47 +0530

    Designed and added layout for html files

--- tasks_todo/views.py
+++ tasks_todo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+tasks = ["foo", "bar", "bat"]
+
+
+def index(request):
+    return render(request, "tasks_todo/index.html", {
+        "tasks": tasks
+    })
+
+
+def add(request):
+    return render(request, "tasks_todo/add.html")
